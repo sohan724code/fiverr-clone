4fbeba1f07d79e1c230a583d6cb0b58496c4f719
sohan724code <sohanmd1375@gmail.com>
2023-05-19 19:34:06 +0600

    first commit

---
+++ src/pages/gig/gig.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./gig.scss";
+
+const gig = () => {
+  return <div className="gig">gig</div>;
+};
+
+export default gig;
---
+++ src/pages/gigs/gigs.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./gigs.scss";
+
+const gigs = () => {
+  return <div className="gigs">gigs</div>;
+};
+
+export default gigs;
